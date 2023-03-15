187bd529d3cde03d860223f46a06993785eec696
gigabyte1511 <gigbyte1511@gmail.com>
2023-03-15 21:29:23 +0600

    Change album by pressing the forward and backward buttons

--- src/components/CoverArtContainer/CoverArtContainer.js
+++ src/components/CoverArtContainer/CoverArtContainer.js
@@ -1,15 +1,26 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import CoverArt from '../CoverArt/CoverArt'
 import styles from './style.module.css'
 
 function CoverArtContainer({ coverArtIDMass }) {
+  const navigate = useNavigate()
   const $scrollWrapper = useRef(null)
+  const [indexCounter, setIndexCounter] = useState(2)
 
+  // Scroll position setup if first render
   useEffect(() => {
     $scrollWrapper.current.scrollLeft = 1550 + 75
-  })
+  }, [])
+
+  // Change detail album component
+  useEffect(() => {
+    navigate('albums', { state: coverArtIDMass[indexCounter].substring(3) })
+  }, [indexCounter])
 
   const $artMass = coverArtIDMass.map((elem) => <CoverArt id={elem} key={elem} />)
+
+  // Change scroll position
   const scrollFnc = (e) => {
     console.log('scroll target', e.target.scrollLeft)
     if (e.target.scrollLeft === 75) {
@@ -19,11 +30,15 @@ function CoverArtContainer({ coverArtIDMass }) {
       e.target.scrollLeft = 1550 + 75
     }
   }
-  const prev = () => {
+  const prevAlbum = () => {
     $scrollWrapper.current.scrollLeft -= 155
+    if (indexCounter <= 0) setIndexCounter(9)
+    else setIndexCounter((prevVal) => prevVal - 1)
   }
-  const next = () => {
+  const nextAlbum = () => {
     $scrollWrapper.current.scrollLeft += 155
+    if (indexCounter >= 9) setIndexCounter(0)
+    else setIndexCounter((prevVal) => prevVal + 1)
   }
   return (
     <div className={styles.container}>
@@ -36,8 +51,8 @@ function CoverArtContainer({ coverArtIDMass }) {
           {[$artMass, $artMass, $artMass]}
         </div>
       </div>
-      <button type="button" className={styles.button_prev} onClick={() => prev()}>{'<'}</button>
-      <button type="button" className={styles.button_next} onClick={() => next()}>{'>'}</button>
+      <button type="button" className={styles.button_prev} onClick={() => prevAlbum()}>{'<'}</button>
+      <button type="button" className={styles.button_next} onClick={() => nextAlbum()}>{'>'}</button>
     </div>
   )
 }
