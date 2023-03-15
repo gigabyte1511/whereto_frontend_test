dbd2eabae4ce02b9bc0a6d706b9e93cf33e6e890
gigabyte1511 <gigbyte1511@gmail.com>
2023-03-16 02:55:46 +0600

    Add smooth scroll of art carusel and album change timeout

--- src/components/CoverArtContainer/CoverArtContainer.js
+++ src/components/CoverArtContainer/CoverArtContainer.js
@@ -6,37 +6,52 @@ import styles from './style.module.css'
 function CoverArtContainer({ coverArtIDMass }) {
   const navigate = useNavigate()
   const $scrollWrapper = useRef(null)
+  const $buttonPrev = useRef(null)
+  const $buttonNext = useRef(null)
   const [indexCounter, setIndexCounter] = useState(2)
+  const [scrollCounter, setScrollCounter] = useState(1550 + 75)
 
-  // Scroll position setup if first render
   useEffect(() => {
-    $scrollWrapper.current.scrollLeft = 1550 + 75
-  }, [])
+    // Prevention of frequent button presses
+    $buttonNext.current.disabled = true
+    $buttonPrev.current.disabled = true
+    setTimeout(() => {
+      $buttonNext.current.disabled = false
+      $buttonPrev.current.disabled = false
+    }, 400)
+    $scrollWrapper.current.scrollLeft = scrollCounter
+    $scrollWrapper.current.style.scrollBehavior = 'smooth'
+  }, [scrollCounter])
 
-  // Change detail album component
+  // Load album info
   useEffect(() => {
-    navigate('albums', { state: coverArtIDMass[indexCounter].substring(3) })
+    setTimeout(() => {
+      navigate('albums', { state: coverArtIDMass[indexCounter].substring(3) })
+    }, 300)
   }, [indexCounter])
 
   const $artMass = coverArtIDMass.map((elem) => <CoverArt id={elem} key={elem} />)
 
-  // Change scroll position
+  // Card carousel loop emulation mechanism
   const scrollFnc = (e) => {
     console.log('scroll target', e.target.scrollLeft)
     if (e.target.scrollLeft === 75) {
-      e.target.scrollLeft = 1550 + 75
+      $scrollWrapper.current.style.scrollBehavior = 'auto'
+      setScrollCounter(1550 + 75)
     }
     if (e.target.scrollLeft >= 3100 + 75) {
-      e.target.scrollLeft = 1550 + 75
+      $scrollWrapper.current.style.scrollBehavior = 'auto'
+      setScrollCounter(1550 + 75)
     }
   }
+  // Change album info
   const prevAlbum = () => {
-    $scrollWrapper.current.scrollLeft -= 155
+    setScrollCounter((prevScrollVal) => prevScrollVal - 155)
     if (indexCounter <= 0) setIndexCounter(9)
     else setIndexCounter((prevVal) => prevVal - 1)
   }
   const nextAlbum = () => {
-    $scrollWrapper.current.scrollLeft += 155
+    setScrollCounter((prevScrollVal) => prevScrollVal + 155)
     if (indexCounter >= 9) setIndexCounter(0)
     else setIndexCounter((prevVal) => prevVal + 1)
   }
@@ -51,8 +66,8 @@ function CoverArtContainer({ coverArtIDMass }) {
           {[$artMass, $artMass, $artMass]}
         </div>
       </div>
-      <button type="button" className={styles.button_prev} onClick={() => prevAlbum()}>{'<'}</button>
-      <button type="button" className={styles.button_next} onClick={() => nextAlbum()}>{'>'}</button>
+      <button ref={$buttonPrev} type="button" className={styles.button_prev} onClick={() => prevAlbum()}>{'<'}</button>
+      <button ref={$buttonNext} type="button" className={styles.button_next} onClick={() => nextAlbum()}>{'>'}</button>
       <div className={styles.left_mask} />
       <div className={styles.right_mask} />
     </div>
